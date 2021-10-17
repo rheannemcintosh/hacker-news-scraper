80a9965169141008ccf05b6c622ac2f58a040c1e
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-10-17 21:23:27 +0100

    Add a second page to scrape

--- scrape.py
+++ scrape.py
@@ -3,11 +3,17 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
-
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 links = soup.select('.storylink')
 subtext = soup.select('.subtext')
+links2 = soup2.select('.storylink')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
 
 def sort_stories_by_votes(hnlist):
     return sorted(hnlist, key = lambda k:k['votes'], reverse=True)
@@ -28,4 +34,4 @@ def create_custom_hn(links, subtext):
         
     return sort_stories_by_votes(hn)
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
